325130cab974404f0eb80fac0e305d2623e7712b
JagadeeshTalari55 <jagadeeshtalari55@gmail.com>
2023-09-05 15:26:08 +0530

    bustickets1 comments added

--- BusTickets.js
+++ BusTickets.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./BusTickets.css";
 
 const BusesAvailable = [
@@ -38,69 +38,91 @@ const BusesAvailable = [
     TotalSeatsAvailable: 21,
     TotalSingleSeats: 5,
   },
+  {
+    Travels: "VSR Tours and Travels",
+    BusTypeString: "A/C Sleeper (2+1)",
+    BusType: "AC",
+    DepatureTime: "20:40",
+    StartingTicketPrice: 1699,
+    TotalSeatsAvailable: 21,
+    TotalSingleSeats: 5,
+  },
 ];
 
-function BusTickets() {
-  const ACBuses = BusesAvailable.filter((bus) => bus.BusType === "AC");
-  const Sleeper = BusesAvailable.filter((bus) => bus.BusType === "Sleeper");
-  const NonAC = BusesAvailable.filter((bus) => bus.BusType === "NON AC");
+function BusTickets2() {
+  const ACBusesArray = BusesAvailable.filter((bus) => bus.BusType === "AC"); //[{ramana toHaveFormValues}, {vsr tours and travels}, {vsr tours and travels} ]
+  const SleeperBusesArray = BusesAvailable.filter(
+    (bus) => bus.BusType === "Sleeper"
+  ); //[{ksm road Lines}, ]
+  const NonACBusesArray = BusesAvailable.filter(
+    (bus) => bus.BusType === "NON AC"
+  ); //[{bharathi travels}]
 
   const [busesTypes, setBusesTypes] = useState(BusesAvailable);
 
+  //   useEffect(callbackfuntion, [dependency array])
+
   return (
-    <div>
-      <div className="BusTypes">
-        <div>
-          <input type="checkbox" id="SEATER" />{" "}
-          <label htmlFor="SEATER">SEATER</label>
+    // Buses Available Total Page
+    <>
+      <h2> Buses Total Page</h2>
+      <div className="BusesTotalPage">
+        {/* Side Bar */}
+        <div className="BusTypes">
+          <div>
+            <input type="checkbox" id="SEATER" />{" "}
+            <label htmlFor="SEATER">SEATER</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="SLEEPER"
+              onChange={() => setBusesTypes(SleeperBusesArray)}
+            />{" "}
+            <label htmlFor="SLEEPER">SLEEPER</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="AC"
+              onChange={() => setBusesTypes(ACBusesArray)}
+            />
+            <label htmlFor="AC">AC</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="NON_AC"
+              onChange={() => setBusesTypes(NonACBusesArray)}
+            />
+            <label htmlFor="NON_AC">NON AC</label>
+          </div>
         </div>
-        <div>
-          <input
-            type="checkbox"
-            id="SLEEPER"
-            onChange={() => setBusesTypes(Sleeper)}
-          />{" "}
-          <label htmlFor="SLEEPER">SLEEPER</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="AC"
-            onChange={() => setBusesTypes(ACBuses)}
-          />
-          <label htmlFor="AC">AC</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="NON_AC"
-            onChange={() => setBusesTypes(NonAC)}
-          />
-          <label htmlFor="NON_AC">NON AC</label>
+        {/* Total Buses Available Cards */}
+        <div className="BusAvailable">
+          {/* Modified Array --> Filter Array ( AC Array, Sleeper Array, Non Ac Array) */}
+          {busesTypes.map((Bus, i) => {
+            return (
+              <div key={i} className="BusDetails">
+                <div>
+                  <h3>{Bus.Travels}</h3>
+                  <div className="busType">{Bus.BusTypeString}</div>
+                </div>
+                <h4>{Bus.DepatureTime}</h4>
+                <p>
+                  Starts from INR
+                  <b>{Bus.StartingTicketPrice}</b>
+                </p>
+                <div>
+                  <p>{Bus.TotalSeatsAvailable} Seats Avaialble</p>
+                  <p>{Bus.TotalSingleSeats} Single</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div className="BusAvailable">
-        {busesTypes.map((Bus, i) => {
-          return (
-            <div key={i} className="BusDetails">
-              <div>
-                <h3>{Bus.Travels}</h3>
-                <div className="busType">{Bus.BusTypeString}</div>
-              </div>
-              <h4>{Bus.DepatureTime}</h4>
-              <p>
-                Starts from INR
-                <b>{Bus.StartingTicketPrice}</b>
-              </p>
-              <div>
-                <p>{Bus.TotalSeatsAvailable} Seats Avaialble</p>
-                <p>{Bus.TotalSingleSeats} Single</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    </>
   );
 }
-export default BusTickets;
+export default BusTickets2;
